6d83b082726cf95071cf4807e23172493feba9c0
Halle Fields <hallekfields@macbookpro.mynetworksettings.com>
2025-01-29 18:02:03 -0500

    mobile styling

--- site/src/App.vue
+++ site/src/App.vue
@@ -1,13 +1,11 @@
 <template>
-  <body>
-    <div class="coming-soon">
-      <h1>coming soon</h1>
-    </div>
-    <div class="keep-up">
-      <h2>
-        keep up with the project on
-        <a href="https://github.com/hallekfields/hallelikeberry/" target="_blank">GitHub</a>
-      </h2>
-    </div>
-  </body>
+  <div class="coming-soon">
+    <h1>coming soon</h1>
+  </div>
+  <div class="keep-up">
+    <h2>
+      keep up with the project on
+      <a href="https://github.com/hallekfields/hallelikeberry/" target="_blank">GitHub</a>
+    </h2>
+  </div>
 </template>
